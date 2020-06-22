3e76f40f21eaf96ec0682a0dcb17d4ac5bf9b951
tomasy <solopea@gmail.com>
2020-06-22 15:54:24 +0800

    clear

--- src/views/config/pluginform.config.js
+++ src/views/config/pluginform.config.js
@@ -1,8 +1,3 @@
-import { BUILDIN_ACTION_CONFIGS } from "../../constant";
-
-export function getActionConfigs() {
-  return JSON.parse(JSON.stringify(BUILDIN_ACTION_CONFIGS))
-}
 
 export function getDefaultForm() {
   return {
